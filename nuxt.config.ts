81062695c92f2f295b045a698a9f53d404fe9c8b
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-04-21 21:31:49 +0800

    install bootstrap css and setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,8 @@ export const config: any = {
   /*
    ** Global CSS
    */
-  css: ['assets/stylesheets/overwrite.styl', 'assets/stylesheets/symbol.styl'],
+  // css: ['assets/stylesheets/overwrite.styl', 'assets/stylesheets/symbol.styl'],
+  css: ['@/assets/stylesheets/main.scss'],
 
   /*
    ** Plugins to load before mounting the App
@@ -44,7 +45,11 @@ export const config: any = {
   /*
    ** Nuxt.js modules
    */
-  modules: ['@nuxtjs/axios', 'nuxt-buefy'].concat(recaptchaPlugin),
+  modules: ['bootstrap-vue/nuxt', '@nuxtjs/axios'].concat(recaptchaPlugin),
+  bootstrapVue: {
+    bootstrapCSS: false,
+    bootstrapVueCSS: false
+  },
 
   buildModules: ['@nuxt/typescript-build'],
 
@@ -61,6 +66,15 @@ export const config: any = {
     siteKey: process.env.RECAPTCHA_CLIENT_SECRET,
     version: 3,
     hideBadge: false
+  },
+
+  module: {
+    rules: [
+        {
+           test: /\.s[ac]ss$/i,
+           use: ['style-loader','css-loader','sass-loader',],
+         },
+    ],
   },
 
   /*
