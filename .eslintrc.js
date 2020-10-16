53fc58d1e808029803a8bfa87d583838d09f70e0
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-10-16 22:28:47 +0800

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,36 @@ module.exports = {
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
   rules: {
-    'no-console': 0
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'always'],
+    'vue/max-attributes-per-line': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/html-indent': ['error', 'tab', {
+        'attribute': 1,
+        'baseIndent': 1,
+        'closeBracket': 0,
+        'alignAttributesVertically': false,
+        'ignores': []
+    }],
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-self-closing': ['error', {'html': {'normal': 'never', 'void': 'always'}}],
+    'curly': ['error', 'multi-or-nest'],
+    'no-var': 'error',
+    'space-before-function-paren': ['error', {
+        'anonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'always'
+    }],
+    'indent': ['error', 'tab'],
+    'no-unused-expressions': 'off',
+    'no-tabs': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'newline-per-chained-call': ['error'],
+    'brace-style': ['error', 'stroustrup'],
+    'newline-after-var': ['error', 'always'],
+
   }
 }
