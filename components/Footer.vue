b497680fbf5b9cd92acab6fb723693894a0a27da
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-11-23 04:52:37 +0800

    Refactor on Client side

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,75 +1,49 @@
 <template>
-  <div class="page-footer">
-    <footer class="footer">
-      <b-container fluid>
-        <b-row class="footer-row mx-0 mx-md-4 mx-lg-4 px-lg-4">
-          <b-col
-            sm="12"
-            lg="4"
-            class="footer-description"
-          >
-            <div class="ft-logo">
-              <a
-                href="https://www.nem.io/"
-                target="_blank"
-              >
-                <img src="~/assets/images/logo_footer.png">
-              </a>
-            </div>
-            <div class="ft-desc mt-3">
-              <p>
-                Symbol is a platform built on revolutionary blockchain technology, reimagining from scratch the ideal
-                blockchain. Symbol does not only allow for standard peer-to-peer value transfer transactions, but instead
-                does much more – store data, make transactions, prove identity, timestamp documents, and create arbitrary
-                digital assets.
-              </p>
-            </div>
-          </b-col>
-          <b-col
-            sm="12"
-            lg="4"
-          >
-            <ul class="social-icon">
-              <li
-                v-for="link in links"
-                :key="link.text"
-                class="social-icon-item"
-              >
-                <a
-                  target="_blank"
-                  :href="link.href"
-                >
-                  <component :is="link.icon" />
-                  <span>{{ link.text }}</span>
-                </a>
-              </li>
-              <li class="social-icon-item">
-                <a
-                  target="_blank"
-                  :href="explorerUrl"
-                >
-                  <IconEarth />
-                  <span>Explorer</span>
-                </a>
-              </li>
-            </ul>
-          </b-col>
+    <div class="page-footer">
+        <footer class="footer">
+            <b-container fluid>
+                <b-row class="footer-row mx-0 mx-md-4 mx-lg-4 px-lg-4">
+                    <b-col sm="12" lg="4" class="footer-description">
+                        <div class="ft-logo">
+                            <a href="https://www.nem.io/" target="_blank">
+                                <img src="~/assets/images/logo_footer.png" />
+                            </a>
+                        </div>
+                        <div class="ft-desc mt-3">
+                            <p>
+                                Symbol is a platform built on revolutionary blockchain technology, reimagining from scratch the ideal
+                                blockchain. Symbol does not only allow for standard peer-to-peer value transfer transactions, but instead
+                                does much more – store data, make transactions, prove identity, timestamp documents, and create arbitrary
+                                digital assets.
+                            </p>
+                        </div>
+                    </b-col>
+                    <b-col sm="12" lg="4">
+                        <ul class="social-icon">
+                            <li v-for="link in links" :key="link.text" class="social-icon-item">
+                                <a target="_blank" :href="link.href">
+                                    <component :is="link.icon" />
+                                    <span>{{ link.text }}</span>
+                                </a>
+                            </li>
+                            <li class="social-icon-item">
+                                <a target="_blank" :href="explorerUrl">
+                                    <IconEarth />
+                                    <span>Explorer</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </b-col>
 
-          <b-col
-            sm="12"
-            lg="4"
-          >
-            <span class="endpoint-info">
-              Node: <a
-                target="_blank"
-                :href="defaultNode"
-              >{{ networkInfo.hostname }}</a>
-            </span>
-          </b-col>
-        </b-row>
-      </b-container>
-    </footer>
-  </div>
+                    <b-col sm="12" lg="4">
+                        <span class="endpoint-info">
+                            Node: <a target="_blank" :href="defaultNode">{{ networkInfo.hostname }}</a>
+                        </span>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -82,139 +56,139 @@ import IconHomeCurrencyUsd from 'vue-material-design-icons/HomeCurrencyUsd.vue';
 import IconEarth from 'vue-material-design-icons/Earth.vue';
 
 export default {
-	components: {
-		IconGithub,
-		IconNewspaper,
-		IconReddit,
-		IconForum,
-		IconTelegram,
-		IconHomeCurrencyUsd,
-		IconEarth,
-	},
-	data() {
-		return {
-			links: [
-				{
-					href: 'https://nemflash.io/',
-					text: 'NEM News',
-					icon: 'IconNewspaper',
-				},
-				{
-					href: 'https://forum.nem.io/',
-					text: 'Forum',
-					icon: 'IconForum',
-				},
-				{
-					href: 'https://t.me/nemred',
-					text: 'Telegram',
-					icon: 'IconTelegram',
-				},
-				{
-					href: 'https://www.reddit.com/r/nem/',
-					text: 'Reddit',
-					icon: 'IconReddit',
-				},
-				{
-					href: 'https://github.com/nemtech',
-					text: 'Github',
-					icon: 'IconGithub',
-				},
-			],
-		};
-	},
-	computed: {
-		networkInfo() {
-			return this.$store.getters.getNetworkInfo;
-		},
-		explorerUrl() {
-			return this.networkInfo.explorerUrl;
-		},
-		defaultNode() {
-			return this.networkInfo.defaultNode;
-		},
-	},
+    components: {
+        IconGithub,
+        IconNewspaper,
+        IconReddit,
+        IconForum,
+        IconTelegram,
+        IconHomeCurrencyUsd,
+        IconEarth,
+    },
+    data() {
+        return {
+            links: [
+                {
+                    href: 'https://nemflash.io/',
+                    text: 'NEM News',
+                    icon: 'IconNewspaper',
+                },
+                {
+                    href: 'https://forum.nem.io/',
+                    text: 'Forum',
+                    icon: 'IconForum',
+                },
+                {
+                    href: 'https://t.me/nemred',
+                    text: 'Telegram',
+                    icon: 'IconTelegram',
+                },
+                {
+                    href: 'https://www.reddit.com/r/nem/',
+                    text: 'Reddit',
+                    icon: 'IconReddit',
+                },
+                {
+                    href: 'https://github.com/nemtech',
+                    text: 'Github',
+                    icon: 'IconGithub',
+                },
+            ],
+        };
+    },
+    computed: {
+        networkInfo() {
+            return this.$store.getters.getNetworkInfo;
+        },
+        explorerUrl() {
+            return this.networkInfo.explorerUrl;
+        },
+        defaultNode() {
+            return this.networkInfo.defaultNode;
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .page-footer {
-	width: 100%;
+    width: 100%;
 }
 
 .footer {
-	width: 100%;
-	bottom: 0;
+    width: 100%;
+    bottom: 0;
 
-	.footer-row {
-		padding-top: 40px;
+    .footer-row {
+        padding-top: 40px;
 
-		.ft-desc {
-			p {
-				font-size: 12px;
-				text-align: justify;
-				color: hsla(0, 0%, 100%, 0.66);
-				font-weight: 200;
-			}
-		}
+        .ft-desc {
+            p {
+                font-size: 12px;
+                text-align: justify;
+                color: hsla(0, 0%, 100%, 0.66);
+                font-weight: 200;
+            }
+        }
 
-		.ft-logo {
-			a {
-				height: 30px;
-				display: inline-block;
+        .ft-logo {
+            a {
+                height: 30px;
+                display: inline-block;
 
-				img {
-					width: auto;
-					max-width: 100%;
-					max-height: 100%;
-				}
-			}
-		}
+                img {
+                    width: auto;
+                    max-width: 100%;
+                    max-height: 100%;
+                }
+            }
+        }
 
-		.footer-description {
-			margin-top: -40px;
-		}
-	}
+        .footer-description {
+            margin-top: -40px;
+        }
+    }
 }
 
 .social-icon {
-	list-style: none;
-	justify-content: flex-start;
-	flex-wrap: wrap;
-	display: flex;
-	padding: 0;
-	margin: 0 -15px;
+    list-style: none;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    display: flex;
+    padding: 0;
+    margin: 0 -15px;
 
-	.social-icon-item {
-		-webkit-box-flex: 1;
-		flex: auto;
-		margin: 5px;
+    .social-icon-item {
+        -webkit-box-flex: 1;
+        flex: auto;
+        margin: 5px;
 
-		a {
-			color: #fff;
-			text-decoration: none;
+        a {
+            color: #fff;
+            text-decoration: none;
 
-			span {
-				margin-left: 5px;
-			}
+            span {
+                margin-left: 5px;
+            }
 
-			i {
-				font-size: 15px;
-			}
-		}
-	}
+            i {
+                font-size: 15px;
+            }
+        }
+    }
 }
 
 .endpoint-info {
-	font-size: small;
-	padding-top: 10%;
-	a {
-		color: #fff;
-	}
+    font-size: small;
+    padding-top: 10%;
+    a {
+        color: #fff;
+    }
 }
 
 .horisontal-center {
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,57 @@
 <template>
-  <b-container
-    fluid="lg"
-    style="max-width: 1200px"
-  >
-    <b-row>
-      <b-col
-        cols="12"
-        lg="6"
-      >
-        <div class="p-3">
-          <b-col>
-            <b-row>
-              <b-img
-                src="~/assets/images/symbol_logo_white.svg"
-                fluid
-                alt="Symbol"
-                width="240"
-              />
-            </b-row>
-            <b-row>
-              <div>
-                <span class="subTitle">Claim mosaics for development and testing purposes on the symbol network</span>
-              </div>
-            </b-row>
+    <b-container fluid="lg" style="max-width: 1200px">
+        <b-row>
+            <b-col cols="12" lg="6">
+                <div class="p-3">
+                    <b-col>
+                        <b-row>
+                            <b-img src="~/assets/images/symbol_logo_white.svg" fluid alt="Symbol" width="240" />
+                        </b-row>
+                        <b-row>
+                            <div>
+                                <span class="subTitle">Claim mosaics for development and testing purposes on the symbol network</span>
+                            </div>
+                        </b-row>
 
-            <b-row>
-              <FaucetForm
-                class="d-lg-none d-xl-none d-md-block mx-form"
-                :mosaic-id="networkInfo.nativeCurrencyId"
-                :filter-mosaics="filterMosaics"
-                :recipient-placeholder="recipientPlaceholder"
-                :amount-placeholder="amountPlaceholder"
-              />
-            </b-row>
+                        <b-row>
+                            <FaucetForm
+                                class="d-lg-none d-xl-none d-md-block mx-form"
+                                :mosaic-id="networkInfo.nativeCurrencyId"
+                                :filter-mosaics="filterMosaics"
+                                :recipient-placeholder="recipientPlaceholder"
+                                :amount-placeholder="amountPlaceholder"
+                            />
+                        </b-row>
 
-            <b-row>
-              <div class="info">
-                <span>Please send back claimed mosaics when you no longer need them.</span>
-                <span>
-                  Faucet Address:
+                        <b-row>
+                            <div class="info">
+                                <span>Please send back claimed mosaics when you no longer need them.</span>
+                                <span>
+                                    Faucet Address:
 
-                  <span class="highlight">
-                    <a
-                      target="_blank"
-                      :href="faucetAccountUrl"
-                    >
-                      {{ networkInfo.address }}
-                    </a>
-                  </span>
-                </span>
-              </div>
-            </b-row>
-          </b-col>
-        </div>
-      </b-col>
+                                    <span class="highlight">
+                                        <a target="_blank" :href="faucetAccountUrl">
+                                            {{ networkInfo.address }}
+                                        </a>
+                                    </span>
+                                </span>
+                            </div>
+                        </b-row>
+                    </b-col>
+                </div>
+            </b-col>
 
-      <b-col lg="6">
-        <FaucetForm
-          class="d-lg-block d-none"
-          :mosaic-id="networkInfo.nativeCurrencyId"
-          :filter-mosaics="filterMosaics"
-          :recipient-placeholder="recipientPlaceholder"
-          :amount-placeholder="amountPlaceholder"
-        />
-      </b-col>
-    </b-row>
-  </b-container>
+            <b-col lg="6">
+                <FaucetForm
+                    class="d-lg-block d-none"
+                    :mosaic-id="networkInfo.nativeCurrencyId"
+                    :filter-mosaics="filterMosaics"
+                    :recipient-placeholder="recipientPlaceholder"
+                    :amount-placeholder="amountPlaceholder"
+                />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -73,76 +59,76 @@ import { Address } from 'symbol-sdk';
 import FaucetForm from '@/components/FaucetForm.vue';
 
 export default {
-	components: {
-		FaucetForm,
-	},
-	computed: {
-		filterMosaics() {
-			return this.$store.getters.getFilterMosaics;
-		},
-		networkInfo() {
-			return this.$store.getters.getNetworkInfo;
-		},
-		recipientPlaceholder() {
-			return `Address start with a capital ${this.networkInfo.address[0]}`;
-		},
-		amountPlaceholder() {
-			return `(Faucet will pay up to ${this.networkInfo.nativeCurrencyMaxOut} XYM, or enter custom amount)`;
-		},
-		faucetAccountUrl() {
-			return `${this.networkInfo.explorerUrl}accounts/${Address.createFromRawAddress(this.networkInfo.address).plain()}`;
-		},
-	},
-	created() {
-		if (process.browser) {
-			this.$store.dispatch('fetchFaucetBalance');
-			// inject method into $nuxt, allow access from store
-			this.$nuxt.$makeToast = this.makeToast;
-		}
-	},
-	methods: {
-		makeToast(variant = null, message, config) {
-			this.$bvToast.toast(message, {
-				title: `Notification`,
-				variant,
-				solid: true,
-				toaster: 'b-toaster-top-right',
-				appendToast: true,
-				...config,
-			});
-		},
-	},
+    components: {
+        FaucetForm,
+    },
+    computed: {
+        filterMosaics() {
+            return this.$store.getters.getFilterMosaics;
+        },
+        networkInfo() {
+            return this.$store.getters.getNetworkInfo;
+        },
+        recipientPlaceholder() {
+            return `Address start with a capital ${this.networkInfo.address[0]}`;
+        },
+        amountPlaceholder() {
+            return `(Faucet will pay up to ${this.networkInfo.nativeCurrencyMaxOut} XYM, or enter custom amount)`;
+        },
+        faucetAccountUrl() {
+            return `${this.networkInfo.explorerUrl}accounts/${Address.createFromRawAddress(this.networkInfo.address).plain()}`;
+        },
+    },
+    created() {
+        if (process.browser) {
+            // this.$store.dispatch('fetchFaucetBalance');
+            // inject method into $nuxt, allow access from store
+            this.$nuxt.$makeToast = this.makeToast;
+        }
+    },
+    methods: {
+        makeToast(variant = null, message, config) {
+            this.$bvToast.toast(message, {
+                title: `Notification`,
+                variant,
+                solid: true,
+                toaster: 'b-toaster-top-right',
+                appendToast: true,
+                ...config,
+            });
+        },
+    },
 };
 </script>
 <style lang="scss" scoped>
 .container {
-	margin-top: 20px;
+    margin-top: 20px;
 }
 
 .row {
-	padding: 10px 0;
+    padding: 10px 0;
 }
 
 .subTitle {
-	font-size: 18px;
-	font-weight: bolder;
+    font-size: 18px;
+    font-weight: bolder;
 }
 
 .mx-form {
-	margin: 0 -0.7rem !important;
-	padding: 0 0.7rem !important;
+    margin: 0 -0.7rem !important;
+    padding: 0 0.7rem !important;
 }
 
 .info {
-	span {
-		display: block;
-		padding: 5px 0;
-		font-size: 14px;
+    span {
+        display: block;
+        padding: 5px 0;
+        font-size: 14px;
 
-		.highlight {
-			display: inline;
-			color: var(--secondary);
-		}
-	}
+        .highlight {
+            display: inline;
+            color: var(--secondary);
+        }
+    }
 }
 </style>
