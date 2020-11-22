833c9632cd87e231e74b67db934871b8da7836e5
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-11-23 04:53:54 +0800

    move init faucet balance into server side

--- server/handlers/faucet.ts
+++ server/handlers/faucet.ts
@@ -1,40 +1,80 @@
-import { MosaicId, MosaicInfo } from 'symbol-sdk';
+import { MosaicId, Mosaic, MosaicInfo, Address, NamespaceId, AccountInfo, Account } from 'symbol-sdk';
 import Url from 'url-parse';
 import { IApp } from '../app';
+import helper from '../helper';
+
+interface Ibalance {
+    mosaicId: string;
+    amount: number;
+    mosaicAliasName: string;
+}
 
 export const faucetHandler = (appConfig: IApp) => {
     return async (_req: any, res: any, next: any) => {
         const { repositoryFactory, config, faucetAccount, isNodeHealth } = appConfig;
 
         if (!isNodeHealth) {
-            res.error = {
-                statusCode: 500,
-                message: `${appConfig.config.DEFAULT_NODE} node is offline.`,
-            };
-
+            res.error = Error(`API node is offline.`);
             return next();
         }
 
-        const nativeMosaicInfo: MosaicInfo = await repositoryFactory
-            .createMosaicRepository()
-            .getMosaic(new MosaicId(config.NATIVE_CURRENCY_ID))
-            .toPromise();
+        try {
+            const defaultNode = new Url(config.DEFAULT_NODE_CLIENT);
 
-        const defaultNode = new Url(config.DEFAULT_NODE_CLIENT);
-        const networkInfo = {
-            address: faucetAccount.address.pretty(),
-            hostname: defaultNode.hostname,
-            defaultNode: defaultNode.origin,
-            nativeCurrencyMaxOut: config.NATIVE_CURRENCY_OUT_MAX / Math.pow(10, nativeMosaicInfo.divisibility),
-            nativeCurrencyName: config.NATIVE_CURRENCY_NAME,
-            nativeCurrencyId: config.NATIVE_CURRENCY_ID,
-            blackListMosaicIds: config.BLACKLIST_MOSAICIDS,
-            explorerUrl: config.EXPLORER_URL,
-        };
+            const [nativeMosaicInfo, accountInfo] = await Promise.all([
+                repositoryFactory.createMosaicRepository().getMosaic(new MosaicId(config.NATIVE_CURRENCY_ID)).toPromise(),
+                repositoryFactory.createAccountRepository().getAccountInfo(faucetAccount.address).toPromise(),
+            ]);
 
-        // Todo: Move fetch Faucet account mosaic
+            const networkInfo = {
+                address: faucetAccount.address.pretty(),
+                hostname: defaultNode.hostname,
+                defaultNode: defaultNode.origin,
+                nativeCurrencyMaxOut: config.NATIVE_CURRENCY_OUT_MAX / Math.pow(10, nativeMosaicInfo.divisibility),
+                nativeCurrencyName: config.NATIVE_CURRENCY_NAME,
+                nativeCurrencyId: config.NATIVE_CURRENCY_ID,
+                blackListMosaicIds: config.BLACKLIST_MOSAICIDS,
+                explorerUrl: config.EXPLORER_URL,
+            };
 
-        res.data = { networkInfo };
+            const resolvedMosaics = await Promise.all(
+                accountInfo.mosaics.map(async (mosaic) => {
+                    let mosaicId: MosaicId = mosaic.id;
+                    if (mosaic instanceof NamespaceId) {
+                        mosaicId = (await repositoryFactory.createNamespaceRepository().getLinkedMosaicId(mosaic).toPromise()) || mosaic.id;
+                    }
+
+                    return new Mosaic(mosaicId, mosaic.amount);
+                }),
+            );
+
+            const resolvedMosaicIds = resolvedMosaics.map((mosaic) => mosaic.id);
+
+            const [mosaicInfos, mosaicNames] = await Promise.all([
+                repositoryFactory.createMosaicRepository().getMosaics(resolvedMosaicIds).toPromise(),
+                repositoryFactory.createNamespaceRepository().getMosaicsNames(resolvedMosaicIds).toPromise(),
+            ]);
+
+            const balance: Ibalance[] = resolvedMosaics.map((mosaic) => {
+                let mosaicInfo: any = mosaicInfos.find((info) => info.id.equals(mosaic.id));
+
+                return {
+                    mosaicId: mosaic.id.toHex(),
+                    amount: helper.toRelativeAmount(mosaic.amount.compact(), mosaicInfo.divisibility),
+                    mosaicAliasName: helper.extractMosaicNamespace(mosaicInfo, mosaicNames),
+                };
+            });
+
+            const faucetBalance: Ibalance[] = balance.filter((mosaic) => !networkInfo.blackListMosaicIds.includes(mosaic.mosaicId));
+
+            res.data = {
+                networkInfo,
+                faucetBalance,
+            };
+        } catch (error) {
+            res.error = Error(`Init faucet server error.`);
+        }
+
         return next();
     };
 };
--- store/index.js
+++ store/index.js
@@ -1,131 +1,90 @@
 import axios from 'axios';
-import { map, concatMap } from 'rxjs/operators';
-import { combineLatest } from 'rxjs';
-import { Listener, Address, RepositoryFactoryHttp, MosaicService } from 'symbol-sdk';
+import { Listener, Address, RepositoryFactoryHttp } from 'symbol-sdk';
 import Vue from 'vue';
 
 export const state = () => ({
-	filterMosaics: [],
-	networkInfo: {},
-	transactionHash: '',
+    filterMosaics: [],
+    networkInfo: {},
+    transactionHash: '',
 });
 
 export const getters = {
-	getNetworkInfo: state => state.networkInfo,
-	getTransactionHash: state => state.transactionHash,
-	getFilterMosaics: state => state.filterMosaics,
+    getNetworkInfo: (state) => state.networkInfo,
+    getTransactionHash: (state) => state.transactionHash,
+    getFilterMosaics: (state) => state.filterMosaics,
 };
 
 export const mutations = {
-	setFilterMosaics: (state, filterMosaics) => {
-		state.filterMosaics = filterMosaics;
-	},
-	setNetworkInfo: (state, networkInfo) => {
-		state.networkInfo = networkInfo;
-	},
-	setTransactionHash: (state, transactionHash) => {
-		state.transactionHash = transactionHash;
-	},
+    setFilterMosaics: (state, filterMosaics) => {
+        state.filterMosaics = filterMosaics;
+    },
+    setNetworkInfo: (state, networkInfo) => {
+        state.networkInfo = networkInfo;
+    },
+    setTransactionHash: (state, transactionHash) => {
+        state.transactionHash = transactionHash;
+    },
 };
 
 export const actions = {
-	nuxtServerInit: ({ commit }, { res, error }) => {
-		if (res.error) {
-			return error(res.error);
-		}
+    nuxtServerInit: ({ commit }, { res, error }) => {
+        if (res.error) {
+            return error(res.error);
+        }
 
-		commit('setNetworkInfo', res.data.networkInfo);
-	},
+        commit('setNetworkInfo', res.data.networkInfo);
+        commit('setFilterMosaics', res.data.faucetBalance);
+    },
 
-	fetchFaucetBalance: context => {
-		const faucetAddress = Address.createFromRawAddress(context.getters.getNetworkInfo.address);
-		const networkInfo = context.getters.getNetworkInfo;
+    claimFaucet: (context, form) => {
+        const recipientAddress = Address.createFromRawAddress(form.recipient);
 
-		const repositoryFactory = new RepositoryFactoryHttp(networkInfo.defaultNode);
-		const mosaicService = new MosaicService(repositoryFactory.createAccountRepository(), repositoryFactory.createMosaicRepository());
+        context.dispatch('openListenser', recipientAddress);
 
-		const mosaicsAmountView = mosaicService.mosaicsAmountViewFromAddress(faucetAddress);
-		const mosaicsNames = mosaicService.mosaicsAmountViewFromAddress(faucetAddress).pipe(
-			map(mosaicsAmountView => mosaicsAmountView.map(mosaic => mosaic.mosaicInfo.id)),
-			concatMap(mosaicIds => repositoryFactory.createNamespaceRepository().getMosaicsNames(mosaicIds)),
-		);
+        axios
+            .post('/claims', { ...form })
+            .then((res) => {
+                context.commit('setTransactionHash', res.data.txHash);
 
-		return combineLatest(mosaicsAmountView, mosaicsNames)
-			.pipe(
-				map(([mosaicsAmountView, mosaicsNames]) => {
-					return mosaicsAmountView.map(mosaicView => {
-						const mosaicName = mosaicsNames.find(name => name.mosaicId.equals(mosaicView.mosaicInfo.id));
+                res.data.mosaics.map((mosaic) => {
+                    Vue.prototype.$nuxt.$makeToast('info', `Mosaic: ${mosaic.name} - Amount: ${mosaic.amount}`);
+                });
+                Vue.prototype.$nuxt.$makeToast('info', `Pending Transaction Hash: ${res.data.txHash}`, {
+                    noAutoHide: true,
+                });
+            })
+            .catch((error) => {
+                console.debug(error);
+                Vue.prototype.$nuxt.$makeToast('warning', `${error.response.data.message}`); // Error!
+            });
+    },
 
-						let mosaicAliasName = '';
+    openListenser: async (context, recipient) => {
+        const networkInfo = context.getters.getNetworkInfo;
+        const wsEndpoint = `${networkInfo.defaultNode.replace('http', 'ws')}/ws`;
+        const repositoryFactory = new RepositoryFactoryHttp(networkInfo.defaultNode);
 
-						if (mosaicName) {
-							mosaicAliasName = mosaicName.names.length > 0 ? mosaicName.names[0].name : mosaicView.mosaicInfo.id.toHex();
-						}
+        const listener = new Listener(wsEndpoint, repositoryFactory.createNamespaceRepository(), WebSocket);
 
-						return {
-							mosaicId: mosaicView.mosaicInfo.id.toHex(),
-							divisibility: mosaicView.mosaicInfo.divisibility,
-							amount: mosaicView.amount.compact() / 10 ** mosaicView.mosaicInfo.divisibility,
-							mosaicAliasName,
-						};
-					});
-				}),
-				map(mosaicList => {
-					return mosaicList.filter(mosaic => !networkInfo.blackListMosaicIds.includes(mosaic.mosaicId));
-				}),
-			)
-			.subscribe(faucet => context.commit('setFilterMosaics', faucet));
-	},
+        await listener.open();
 
-	claimFaucet: (context, form) => {
-		const recipientAddress = Address.createFromRawAddress(form.recipient);
+        listener.unconfirmedAdded(recipient).subscribe((response) => {
+            if (context.getters.getTransactionHash === response.transactionInfo.hash) {
+                Vue.prototype.$nuxt.$makeToast('success', `Your request is being processed.`);
+            }
+        });
 
-		context.dispatch('openListenser', recipientAddress);
+        listener.confirmed(recipient).subscribe((response) => {
+            if (context.getters.getTransactionHash === response.transactionInfo.hash) {
+                Vue.prototype.$nuxt.$makeToast('success', `Your request has been processed.`);
+                Vue.prototype.$nuxt.$makeToast('success', `View transaction in explorer.`, {
+                    noAutoHide: true,
+                    href: `${networkInfo.explorerUrl}transactions/${response.transactionInfo.hash}`,
+                });
 
-		axios
-			.post('/claims', { ...form })
-			.then(res => {
-				context.commit('setTransactionHash', res.data.txHash);
-
-				res.data.mosaics.map(mosaic => {
-					Vue.prototype.$nuxt.$makeToast('info', `Mosaic: ${mosaic.name} - Amount: ${mosaic.amount}`);
-				});
-				Vue.prototype.$nuxt.$makeToast('info', `Pending Transaction Hash: ${res.data.txHash}`, {
-					noAutoHide: true,
-				});
-			})
-			.catch(error => {
-				console.debug(error);
-				Vue.prototype.$nuxt.$makeToast('warning', `${error.response.data.message}`); // Error!
-			});
-	},
-
-	openListenser: async (context, recipient) => {
-		const networkInfo = context.getters.getNetworkInfo;
-		const wsEndpoint = `${networkInfo.defaultNode.replace('http', 'ws')}/ws`;
-		const repositoryFactory = new RepositoryFactoryHttp(networkInfo.defaultNode);
-
-		const listener = new Listener(wsEndpoint, repositoryFactory.createNamespaceRepository(), WebSocket);
-
-		await listener.open();
-
-		listener.unconfirmedAdded(recipient).subscribe(response => {
-			if (context.getters.getTransactionHash === response.transactionInfo.hash)
-				{Vue.prototype.$nuxt.$makeToast('success', `Your request is being processed.`);}
-		});
-
-		listener.confirmed(recipient).subscribe(response => {
-			if (context.getters.getTransactionHash === response.transactionInfo.hash) {
-				Vue.prototype.$nuxt.$makeToast('success', `Your request has been processed.`);
-				Vue.prototype.$nuxt.$makeToast('success', `View transaction in explorer.`, {
-					noAutoHide: true,
-					href: `${networkInfo.explorerUrl}transactions/${response.transactionInfo.hash}`,
-				});
-
-				context.commit('setTransactionHash', '');
-				listener.close();
-				context.dispatch('fetchFaucetBalance');
-			}
-		});
-	},
+                listener.close();
+                context.dispatch('fetchFaucetBalance');
+            }
+        });
+    },
 };
